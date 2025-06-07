25cba1fd3161b209ea7df76cce7250e20c5a405f
aflamiano <aflamiano.career@gmail.com>
2025-06-07 12:20:37 +0800

    fix/dashboard: relative path fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   useEffect(function () {
     async function getActivities() {
       try {
-        const res = await fetch("data/data.json");
+        const res = await fetch("/data/data.json");
         if (!res.ok) throw new Error("No response");
         const data = await res.json();
         setActivities(data);
